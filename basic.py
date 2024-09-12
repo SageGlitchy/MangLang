0007ee7b010f88478fc33171abbd0acfac21012f
Sage Glitchy <sageglitchy@gmail.com>
2024-09-13 00:30:46 +0530

    Day-1

--- basic.py
+++ basic.py
@@ -1,7 +1,30 @@
-######TOKENS######
+######################################
+# CONSTANTS
+######################################
 
-TT_INT='TT_INT'
-TT_FLOAT='TT_FLOAT'
+DIGITS = '0123456789'
+
+######################################
+# ERROR
+######################################
+class Error:
+    def __init__(self, error_name, details):
+        self.error_name=error_name
+        self.details=details
+    
+    def as_string(self):
+        result=f'{self.error_name}: {self.details}'
+
+class IllegalCharError(Error):
+    def __init__(self, details):
+        super().__init__('Illegal Character', details)
+      
+######################################
+#   TOKENS
+######################################
+
+TT_INT='INT'
+TT_FLOAT='FLOAT'
 TT_PLUS='PLUS'
 TT_MINUS='MINUS'
 TT_MUL='MUL'
@@ -10,18 +33,16 @@ TT_LPAREN='LPAREN'
 TT_RPAREN='RPAREN'
 
 
-
-class token:
-    def __init__(self, type_, value):
+class Token:
+    def __init__(self, type_, value=None):
         self.type = type_
         self.value= value
-        
-def __repr__(self):
-    if self.value: return f'{self.type}:{self.value}'
-    return f'{self.type}'
+            
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
 
-######LEXER######
-
+#      LEXER
 class Lexer:
     def __init__(self,text):
         self.text = text
@@ -36,8 +57,13 @@ class Lexer:
     def make_tokens(self):
         tokens=[]
         while self.current_char!=None:
-            if self.current_char=='+':
+            if self.current_char in '\t':
+                self.advance()
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+            elif self.current_char=='+':
                 tokens.append(Token(TT_PLUS))
+                self.advance()
             elif self.current_char=='-':
                 tokens.append(Token(TT_MINUS))
                 self.advance()
@@ -53,23 +79,40 @@ class Lexer:
             elif self.current_char==')':
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
-            
-        return tokens
- 
-def make_number(self):
-    num_str=''
-    dot_count=0
+            else:
+                char=self.current_char
+                self.advance()
+                return [], IllegalCharError("'" + char + "'")
+        return tokens, None
     
-    while self.current_char != None and self.current_char in DIGITS + '.':
-        if self.current_char == '.':
-            if dot_count == 1: break
-            dot_count+=1
-            num_str+='.'
+    def make_number(self):
+        num_str=''
+        dot_count=0
+        
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: break
+                dot_count+=1
+                num_str+='.'
+            else:
+                num_str+=self.current_char
+            self.advance()
+        
+        if dot_count==0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str))
 
 
+####################################
+#RUN
+####################################
 
-
-
+def run(text):
+    lexer=Lexer(text)
+    tokens, error= lexer.make_tokens()
+    
+    return tokens, error
 
 
 
